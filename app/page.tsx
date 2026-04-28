9205209e2a510aad3e8253d2d40b12b77f2ee518
Zain Manzoor <zainmanzoor@foomotion.io>
2026-04-28 21:30:16 +0500

    remove scroll to section

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,9 @@ import BoostSales from './components/BoostSales'
 import SmartMarketing from './components/SmartMarketing'
 import SavingsCalculator from './components/SavingsCalculator'
 import CutCosts from './components/CutCosts'
-import FullPageScroll from './components/FullPageScroll'
 export default function Home() {
   return (
     <>
-      <FullPageScroll />
       <main className="flex-1 bg-white">
         <Header />
         <HowItWorks />
